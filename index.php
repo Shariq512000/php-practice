6559f6846c5a6fa8c69a9a61ffbe9b217af1ce05
Shariq Siddiqui <99816206+Shariq512000@users.noreply.github.com>
2023-11-04 04:29:58 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+ //echo "<h1>Hello World</h1><br><h2>Hello World</h2><br><h3>Hello World</h3><br><h4>Hello World</h4><br><h5>Hello World</h5><br><h6>Hello World</h6><br><p>Hello World</p><br><span>Hello World</span><br><em>Hello World</em> ";
+ //$name = "shariq";
+ //$num = 1;
+ //echo "$name $num";
+ //$num1 = 5;
+ //$num2 = 8;
+ //$numResult = $num1 + $num2;
+ //echo $numResult;
+
+ //$num1 = 5;
+ //$num2 = 2;
+ //$powerOfNum = pow($num1 , $num2);
+ //echo $powerOfNum;
+
+ //$num1 = 5;
+ //$num2 = 32;
+ //$filter = $num2 % $num1;
+ //echo $filter;
+?>
